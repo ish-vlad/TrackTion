498f119a74d4dbb3d14bc48acb2af5fe90a0c0e7
rollovd <rollov23@gmail.com>
2021-01-14 20:10:36 +0300

    threshold has increased to 0.75

--- yolov3/vars.py
+++ yolov3/vars.py
@@ -2,7 +2,7 @@ class Yolo:
     LABELS_FILE = 'darknet/data/coco.names'
     CONFIG_FILE = 'darknet/cfg/yolov3.cfg'
     WEIGHTS_FILE = 'darknet/yolov3.weights.txt'
-    CONFIDENCE_THRESHOLD = 0.3
+    CONFIDENCE_THRESHOLD = 0.75
 
 class Image:
     IMG_SHAPE_X = 384
