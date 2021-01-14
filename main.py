91a2e0a494445e21ab254c160fbbc8454a55e9c3
rollovd <rollov23@gmail.com>
2021-01-14 19:07:29 +0300

    get time of program's execution

--- main.py
+++ main.py
@@ -1,13 +1,17 @@
 from yolov3.yolo import object_tracking_table
+import time
 
 if __name__ == "__main__":
     INPUT_FILE = 'ThreePastShop1cor-cut.mp4'
     output_name = INPUT_FILE.split('.')[0]
     OUTPUT_FILE = output_name + '-output.avi'
 
+    start_time = time.time()
     frame = object_tracking_table(
         INPUT_FILE, # Входное видео
         OUTPUT_FILE, # Видео с разметкой
         show_output_image=False, # Отображение каждого фрейма с детекцией
         save_csv=True # CSV-файл сохраняется в папку csv-files
     )
+
+    print("--- %s seconds ---" % (time.time() - start_time))
