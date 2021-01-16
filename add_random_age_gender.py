2ba1d46419cce736b207f0ff1623454e5af0cf30
Pavel Karpyshev <p.karpyshev@gmail.com>
2021-01-16 23:37:24 +0300

    Moved to function

--- add_random_age_gender.py
+++ add_random_age_gender.py
@@ -6,13 +6,7 @@ GENDERS = ['Male', 'Female']
 AGES = ['0-18', '19-25', '26-40', '41-60', '60+']
 
 
-import pandas as pd
-if __name__ == "__main__":
-    IN_FNAME = sys.argv[1]
-    OUT_FNAME = sys.argv[2]
-    
-    df = pd.read_csv(IN_FNAME, index_col=0)
-    
+def add_random_age_gender(df):
     genders = {}
     ages = {}
     for i in df['id'].unique():
@@ -21,5 +15,17 @@ if __name__ == "__main__":
 
     df['gender'] = df['id'].apply(lambda x: genders[x])
     df['age'] = df['id'].apply(lambda x: ages[x])
+
+    return df
+
+
+import pandas as pd
+if __name__ == "__main__":
+    IN_FNAME = sys.argv[1]
+    OUT_FNAME = sys.argv[2]
+    
+    df = pd.read_csv(IN_FNAME, index_col=0)
+    
+    df = add_random_age_gender(df)
 
     df.to_csv(OUT_FNAME)
